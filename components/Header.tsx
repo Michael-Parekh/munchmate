c59f7be683c5a06d1ea934e7191622692e9e2d07
suchitb12 <sbandaram08@gmail.com>
2023-11-10 15:08:45 -0600

    Test push

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function Header() {
     return (
-      <></>
+      <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
     )
   }
