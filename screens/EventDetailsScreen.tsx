abbf4fba79018fb97ce3c290bd0d206f9b1cd362
Yichen Huang <yichen41@illinois.edu>
2023-11-17 02:32:55 -0600

    Successfully displayed the props onto page

--- screens/EventDetailsScreen.tsx
+++ screens/EventDetailsScreen.tsx
@@ -1,19 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { useRoute, RouteProp } from "@react-navigation/native";
+import { StackParamList } from "../constants";
 
 const EventDetailsScreen: React.FC = () => {
+  const route = useRoute<RouteProp<StackParamList, 'EventDetail'>>();
+  const { eventTitle, eventDate } = route.params;
+
   return (
-    <View style={styles.view}>
-      <Text>EventDetails Screen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.view}>
+        <Text style={styles.title}>{eventTitle}</Text>
+        <Text style={styles.date}>{eventDate}</Text>
+        {/* Display more details passed as parameters */}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {
+  container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  view: {
+
+  },
+  title: {
+
+  },
+  date: {
+
   },
 });
 
